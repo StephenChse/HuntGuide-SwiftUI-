d5434450e9d4c8ac30b09c81df5ed5e5e4ba9eab
StephenChse <haku.river@hotmail.co.uk>
2024-11-29 13:31:31 +0000

    Add files via upload

---
+++ HuntGuide(SwiftUI)/HuntGuide(SwiftUI)/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HuntGuide(SwiftUI)
+//
+//  Created by Stephen Chase on 29/11/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HuntGuide(SwiftUI)/HuntGuide(SwiftUI)/HuntGuide_SwiftUI_App.swift
@@ -0,0 +1,17 @@
+//
+//  HuntGuide_SwiftUI_App.swift
+//  HuntGuide(SwiftUI)
+//
+//  Created by Stephen Chase on 29/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct HuntGuide_SwiftUI_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
